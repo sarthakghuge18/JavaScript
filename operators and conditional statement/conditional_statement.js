98643f5fdec1661f70d7f9185446776e3ae8c6b5
sarthakghuge18 <ghugesarthak7@gmail.com>
2024-04-12 09:54:42 +0530

    js

---
+++ operators and conditional statement/conditional_statement.js
@@ -0,0 +1,38 @@
+
+//conditional statement 
+console.log("conditional statement");
+
+// if statement 
+
+let age = 15;
+
+if(age > 18 ){
+    console.log("you can vote");
+}
+
+if(age < 18 ){
+    console.log("you cannot vote");
+}
+
+
+// if else statement 
+
+if(age > 18 ){
+    console.log("you can vote");
+}else{
+    console.log("you cannot vote");
+}
+
+// else if statement 
+
+if(age < 18 ){
+    console.log("junior");
+}else if(age > 60){
+    console.log("senior");
+}else{
+    console.log("middle");
+}
+
+
+ let name = prompt("name");
+ console.log(name);
---
+++ operators and conditional statement/operators.js
@@ -0,0 +1,78 @@
+
+/*
+
+operator used to perform some operation on data
+
+        1) Arithmetic operators ( + , - , * , / , % )
+
+*/
+
+console.log("Arithmetic Operators -->")
+let a = 12;
+let b = 4;
+console.log("a = ", a , " b = ", b);
+console.log("a + b = ", a + b );
+console.log("a - b = ", a - b );
+console.log("a * b = ", a * b );
+console.log("a / b = ", a / b );
+console.log("a % b = ", a % b );      //modulus 
+console.log("a ** b = ", a ** b );      //exponentiation
+console.log("++a = ", ++a );            //increment 
+console.log("--b = ", --b );            //decrement
+
+
+/*
+
+        2) Assignment operators ( = , += , -= , *= , )
+ */
+        console.log("Assignment Operators --> ")
+        console.log("a = ", a , " b = ", b);
+        console.log("a += b ", a += b );
+        console.log("a -= b ", a -= b );
+        console.log("a *= b ", a *= b );
+        console.log("a /= b ", a /= b );
+        console.log("a %= b ", a %= b );
+
+/*
+
+        3) Comparision operator (== , < , > , <= , >= , != , ===)
+ */
+
+
+    console.log("comparision operators -->")
+    a = 6;
+    b = 7;
+    console.log("a = ",a , " b = ",b);
+    console.log("a == b ",a == b);
+    console.log("a <= b ",a <= b);
+    console.log("a >= b ",a >= b);
+    console.log("a != b ",a != b);
+    console.log("a === b ",a === b);
+    console.log("a !== b ",a !== b);
+    
+
+/*
+
+        4) logical operator ( && , || , !)
+ */
+
+        a = 6;
+        b = 8;
+
+        console.log("logical operators -->")
+        let cond1 = a < b ;
+        let cond2 = a > b; 
+        console.log("cond1 = ",cond1 );
+        console.log("cond2 = ",cond2 );
+        console.log("cond1 && cond2 = ",cond1 && cond2);
+        console.log("cond1 || cond2 = ",cond1 || cond2);
+        console.log("!cond1 = ",!cond1 );
+        console.log("!cond2 = ",!cond2 );
+
+
+
+        
+        
+
+
+
