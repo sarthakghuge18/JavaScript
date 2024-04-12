4d5a9646bac2bfd932b0728a0974183e825b0b17
sarthakghuge18 <ghugesarthak7@gmail.com>
2024-04-12 08:26:24 +0530

    js

---
+++ variable and datatype/variable.js
@@ -0,0 +1,54 @@
+fullName = "sarthak ghuge";   //string datatype
+age = 20;                       //integer
+price = 99.99;                  //float
+x = null;
+y = undefined;
+console.log(fullName);
+
+isFollow = false;          //boolean
+console.log(isFollow);
+
+
+//we create variable with keyword 
+            //var const & let
+
+            /*
+            
+            1) var - variable can be redeclared and update . global scope varible .
+
+            
+            2)let - varible cannot be redeclared but can be update . block scope varible.
+            
+            
+            3)const - varible cannot be redeclared or update . block scope varible.
+            
+            
+            */
+
+
+
+var m = 20;
+var m = 30;
+var m = 70;
+console.log(m);
+
+
+let n = 20;
+n = 30;
+n = 89;
+console.log(n);
+
+const p = 20;
+console.log(p);
+
+
+//Datatypes -->
+/*
+number 
+string 
+boolean
+undefined
+null
+bigint
+symbol
+ */
